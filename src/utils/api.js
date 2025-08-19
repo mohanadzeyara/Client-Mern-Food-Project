c1df7bbff5f1e57429252a1a0b4c7fb9bf304756
mohanadzeyara <mzeyara752.2@gmail.com>
2025-08-19 04:46:42 +0200

    to deploy

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
+
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000'
+  baseURL: process.env.REACT_APP_API_URL || 'https://server-mern-food-project-1.onrender.com'
 });
+
 export default api;
